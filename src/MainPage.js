66c9006cbbb81de8eaedf267f04008063595e959
Trang Nguyen <ntttrang2130@gmail.com>
2022-05-18 12:00:12 -0400

    add static mainpage

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import './MainPage.css'
 export default function MainPage() {
   return (
     <main>
-      <h1>Top Questions</h1>
-      <button>Ask Question</button>
+      <div className="headerrow">
+        <h1>Top Questions</h1>
+        <button>Ask&nbsp;Question</button>
+      </div>
+      <div className='questionrow'>
+        <div className='questionstats'>0<span>votes</span></div>
+        <div className='questionstats'>1<span>answers</span></div>
+        <div className='questionstats'>6<span>views</span></div>
+        <div className='questiontitle'>
+          <div className='questionlink'>Hello</div>
+          <div className='tags'>Tags</div>
+        </div>
+      </div>
     </main>
   )
 }
